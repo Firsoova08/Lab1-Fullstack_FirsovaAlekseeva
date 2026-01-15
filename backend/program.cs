7dad5c2be415f1ae16a05bc468bf4f8d1e6bc67f
Firsoova08 <foxalishca@mail.ru>
2026-01-15 21:29:43 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Фирсова Алиса Антонвна");
+Console.WriteLine("Алексеева Валерия Денисовна");
+Console.WriteLine(DateTime.Now);
+
